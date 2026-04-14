ae6aab89d4985e4182c6421726f35111a7431f88
Juan Lee <me@juany.kr>
2026-04-14 11:46:39 +0900

    Add support for autoscaling services in ECS configuration

--- locals.tf
+++ locals.tf
@@ -6,4 +6,8 @@ locals {
   task_execution_role_arn = var.create_task_execution_role ? aws_iam_role.task_execution[0].arn : var.task_execution_role_arn
   task_role_arn           = var.create_task_role ? aws_iam_role.task[0].arn : var.task_role_arn
   security_group_ids      = var.create_security_group ? [aws_security_group.this[0].id] : var.security_groups
+  autoscaling_services = {
+    for entry in var.services : entry.name => entry
+    if try(entry.autoscaling.enabled, false)
+  }
 }
--- main.tf
+++ main.tf
@@ -198,3 +198,33 @@ resource "aws_ecs_service" "this" {
 
   tags = local.resolved_tags
 }
+
+resource "aws_appautoscaling_target" "this" {
+  for_each = local.autoscaling_services
+
+  max_capacity       = lookup(each.value.autoscaling, "max_capacity", lookup(each.value, "desired_count", 1))
+  min_capacity       = lookup(each.value.autoscaling, "min_capacity", 1)
+  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this[each.key].name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu_target_tracking" {
+  for_each = local.autoscaling_services
+
+  name               = "${each.key}-cpu-target-tracking"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.this[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.this[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.this[each.key].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = lookup(each.value.autoscaling, "target_cpu_utilization", 70)
+    scale_in_cooldown  = lookup(each.value.autoscaling, "scale_in_cooldown", 300)
+    scale_out_cooldown = lookup(each.value.autoscaling, "scale_out_cooldown", 60)
+  }
+}
